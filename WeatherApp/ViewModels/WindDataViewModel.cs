5aecdc364246506bf57fd34d81ded8ac939ccefe
furytorial <william-la@hotmail.com>
2021-11-13 15:00:58 -0500

    wpf et ViewModel complété

--- WeatherApp/ViewModels/WindDataViewModel.cs
+++ WeatherApp/ViewModels/WindDataViewModel.cs
@@ -4,15 +4,35 @@ using WeatherApp.Services;
 
 namespace WeatherApp.ViewModels
 {
-    public class WindDataViewModel
+    public class WindDataViewModel : BaseViewModel
     {
         public IWindDataService WindDataService { get; set; }
         public DelegateCommand<string> GetDataCommand { get; set; }
-        public WindDataModel CurrentData { get; set; }
+        private WindDataModel currentData;
+
+        public WindDataModel CurrentData
+        {
+            get => currentData;
+
+            set { currentData = value; OnPropertyChanged(); }
+        }
 
         public double KPHtoMPS(double kph) => kph * 1.0 / 36.0;
         public double MPStoKPH(double mps) => mps * 3.6;
-        
+
+        public WindDataViewModel()
+        {
+            WindDataService = new OpenWeatherService(AppConfiguration.GetValue("ApiKey"));
+            GetDataCommand = new DelegateCommand<string>(GetData);
+        }
+
+
+
+        private async void GetData(string s)
+        {
+            CurrentData = await WindDataService.GetDataAsync();
+        }
+
 
     }
 }
--- WeatherApp/Views/MainWindow.xaml.cs
+++ WeatherApp/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using OpenWeatherAPI;
+using System.Windows;
 using WeatherApp.ViewModels;
 
 namespace WeatherApp
@@ -8,7 +9,7 @@ namespace WeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        TemperatureViewModel vm;
+        WindDataViewModel vm;
 
         public MainWindow()
         {
@@ -16,9 +17,11 @@ namespace WeatherApp
 
             /// TODO : Faire les appels de configuration ici ainsi que l'initialisation
 
-            vm = new TemperatureViewModel();
+            vm = new WindDataViewModel();
 
-            DataContext = vm;           
+            ApiHelper.InitializeClient();
+
+            DataContext = vm;
         }
     }
 }
