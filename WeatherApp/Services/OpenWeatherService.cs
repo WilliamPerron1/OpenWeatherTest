fcbf3410f3f11885231a86525534468139f405be
furytorial <william-la@hotmail.com>
2021-11-13 13:11:53 -0500

    OpenWeatherService complété 1769535

---
+++ WeatherApp/Services/OpenWeatherService.cs
@@ -0,0 +1,38 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class OpenWeatherService : IWindDataService
+    {
+        private static OpenWeatherProcessor owp;
+
+        public WindDataModel LastWindData;
+        public async Task<WindDataModel> GetDataAsync()
+        {
+            OWCurrentWeaterModel gg = await owp.GetCurrentWeatherAsync();
+
+            WindDataModel wdm = new WindDataModel();
+
+            wdm.DateTime = DateTime.UnixEpoch.AddSeconds(gg.DateTime);
+            wdm.Direction = gg.Wind.Deg;
+            wdm.MetrePerSec = gg.Wind.Speed;
+
+            LastWindData = wdm;
+            return LastWindData;
+
+        }
+
+        public OpenWeatherService (string apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+
+            owp.ApiKey = apiKey;
+        }
+    }
+}
